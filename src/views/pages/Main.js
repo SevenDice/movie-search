0ee70e1fc2b7d980197a4db6688cd5918b8ce7af
SevenDice <benford.mark783@gmail.com>
2020-04-29 17:35:40 +0400

    feat: add html swiper

--- src/views/pages/Main.js
+++ src/views/pages/Main.js
@@ -8,7 +8,15 @@ const Main = {
           <span class="search-tia"></span>
         </form>
         <p class="info"></p>
-        <div class="swiper-outer"></div>
+      <div class="swiper-outer">
+        <div class="swiper-container swiper-initialized swiper-container-horizontal">
+        <div class="swiper-wrapper"></div>
+        <div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets"></div>
+        <span class="swiper-notification"></span>
+        </div>
+        <div class="swiper-btn swiper-btn-next" tabindex="0" role="button" aria-label="Next slide"></div>
+        <div class="swiper-btn swiper-btn-prev" tabindex="0" role="button" aria-label="Previous slide"></div>
+      </div>
       `
     return view
   },
